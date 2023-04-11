d5d72cef7f60ef2d69b0ec0198674832a3e9d331
henning410 <henning1.weise@web.de>
2023-04-11 17:36:39 +0200

    added search bar

--- src/app/osm-map2/osm-map2.page.ts
+++ src/app/osm-map2/osm-map2.page.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 import * as L from 'leaflet';
 import {antPath} from 'leaflet-ant-path';
+import {GeoSearchControl, OpenStreetMapProvider} from "leaflet-geosearch";
 
 
 @Component({
@@ -19,8 +20,36 @@ export class OsmMap2Page implements OnInit {
 
   greenIcon = L.icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/2017/2017809.png',
-    iconSize:     [40, 40], // size of the icon
+    iconSize: [40, 40], // size of the icon
   });
+
+  geojson: any = {
+    "type": "Feature",
+    "properties": {
+      "place_id": 308130537,
+      "osm_type": "relation",
+      "osm_id": 2792882,
+      "display_name": "Esslingen am Neckar, Landkreis Esslingen, Baden-Württemberg, Deutschland",
+      "place_rank": 16,
+      "category": "boundary",
+      "type": "administrative",
+      "importance": 0.6154241031206532,
+      "icon": "https://nominatim.openstreetmap.org/ui/mapicons/poi_boundary_administrative.p.20.png"
+    },
+    "bbox": [
+      9.2562418,
+      48.7065634,
+      9.4165802,
+      48.7757682
+    ],
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        9.3071685,
+        48.7427584
+      ]
+    }
+  }
 
   constructor() {
   }
@@ -42,9 +71,21 @@ export class OsmMap2Page implements OnInit {
     //center marker when popup opens
     this.map.on('popupopen', (e) => {
       var px = this.map!.project(e.target._popup._latlng); // find the pixel location on the map where the popup anchor is
-      px.y -= e.target._popup._container.clientHeight/2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
-      this.map!.panTo(this.map!.unproject(px),{animate: true}); // pan to new center
+      px.y -= e.target._popup._container.clientHeight / 2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
+      this.map!.panTo(this.map!.unproject(px), {animate: true}); // pan to new center
     });
+
+    const geoJsonLayer = L.geoJSON(this.geojson, {
+
+      pointToLayer(feature, latlng) {
+        const greenIcon = L.icon({
+          iconUrl: 'https://cdn-icons-png.flaticon.com/512/2017/2017809.png',
+          iconSize: [40, 40], // size of the icon
+        });
+        return L.marker(latlng, {icon: greenIcon});
+      }
+    }).addTo(this.map);
+
 
     /*antPath([[28.644800, 77.216721], [34.1526, 77.5771]],
       {color: '#FF0000', weight: 5, opacity: 0.6})
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {Component} from '@angular/core';
+import {IonicModule} from '@ionic/angular';
+import {ExploreContainerComponent} from '../explore-container/explore-container.component';
 import {Tab2Page} from "../tab2/tab2.page";
-import {OsmMapPage} from "../osm-map/osm-map.page";
 import {OsmMap2Page} from "../osm-map2/osm-map2.page";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent, Tab2Page, OsmMap2Page],
+  imports: [IonicModule, ExploreContainerComponent, Tab2Page, OsmMap2Page, HttpClientModule, CommonModule, FormsModule],
 })
 export class Tab1Page {
-  constructor() {}
+  result = ""
+  selectedLocation = null;
+  config: any = null;
+
+  constructor(private http: HttpClient) {
+  }
+
+  handleChange(event: any) {
+    console.log('CHANGE INPUT');
+    this.showConfig();
+    this.result = event.target.value.toLowerCase();
+  }
+
+  showConfig() {
+    this.getRequest()
+      .subscribe((data: any) => this.config = data.features);
+  }
+
+  getRequest() {
+    return this.http.get<JSON>('https://nominatim.openstreetmap.org/search?city=' + this.result +'&format=geojson');
+  }
+
+  itemSelected(item: any) {
+    this.config = [];
+    this.result = item.properties.display_name;
+    this.selectedLocation = item;
+  }
 }
+
